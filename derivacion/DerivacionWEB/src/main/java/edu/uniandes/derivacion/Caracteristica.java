1abd8a5467c9b9264acb27055017dbd191cd37ed
camilocerquera <jc.cerquera10@uniandes.edu.co>
2015-05-10 03:22:10 -0500

    commit inicial proyecto web derivacion

---
+++ derivacion/DerivacionWEB/src/main/java/edu/uniandes/derivacion/Caracteristica.java
@@ -0,0 +1,31 @@
+package edu.uniandes.derivacion;
+
+public class Caracteristica {
+	private String nombre;
+	private Boolean seleccionada;
+	private String codigo;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Boolean getSeleccionada() {
+		return seleccionada;
+	}
+
+	public void setSeleccionada(Boolean seleccionada) {
+		this.seleccionada = seleccionada;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+}
---
+++ derivacion/DerivacionWEB/src/main/java/edu/uniandes/derivacion/enums/Caracteristicas.java
@@ -0,0 +1,20 @@
+package edu.uniandes.derivacion.enums;
+
+public enum Caracteristicas {
+	REDES_SOCIALES(Boolean.FALSE), FACEBOOK(Boolean.FALSE), TWITTER(
+			Boolean.FALSE);
+
+	private Boolean seleccionada;
+
+	private Caracteristicas(Boolean seleccionada) {
+		this.seleccionada = seleccionada;
+	}
+
+	public Boolean getSeleccionada() {
+		return seleccionada;
+	}
+
+	public void setSeleccionada(Boolean seleccionada) {
+		this.seleccionada = seleccionada;
+	}
+}
