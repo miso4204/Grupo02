9891c945a71c19a87bfd62766334560d7efde7c4
vp.salcedo93 <vp.salcedo93@MISOEST-26.virtual.uniandes.edu.co>
2015-05-13 15:11:46 -0500

    Recordar modificar Rutas en Constantes de ReportEJB.

--- estampate/EstampateReportEJB/src/main/java/edu/uniandes/services/util/Constantes.java
+++ estampate/EstampateReportEJB/src/main/java/edu/uniandes/services/util/Constantes.java
@@ -2,6 +2,6 @@ package edu.uniandes.services.util;
 
 public class Constantes {
 
-	public static final String REPORTES_RUTA="C:\\proyecto\\reportes\\";
-	public static final String PDF_RUTA="C:\\Users\\template\\Desktop\\Vabrik\\Private Tunnel.lnk\\estampate\\estampateWEBAvanzado\\src\\main\\webapp\\public\\";
+	public static final String REPORTES_RUTA="C:\\Users\\template\\Desktop\\Vabrik\\Private Tunnel.lnk\\estampate\\EstampateReportEJB\\src\\main\\java\\edu\\uniandes\\services\\implement\\";
+	public static final String PDF_RUTA="C:\\Users\\template\\Desktop\\eclipse\\eclipse\\jboss-eap-6.3\\welcome-content\\";
 }
